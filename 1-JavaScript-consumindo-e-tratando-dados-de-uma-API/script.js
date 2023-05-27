18a2999adda0c434cc09b84ba9ac02afaf65a4a3
EduardoDamSilva <eduardo.silva@sptech.school>
2023-05-26 23:54:17 -0300

    primeiro commit do curso de asincronicidade

---
+++ 1-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
@@ -0,0 +1,49 @@
+//const consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+  // Essa linha serve para tratar os dados que vem como binário para colocar em JSON
+//  .then((reposta) => reposta.json()) 
+  // Arraw function que pega a resposta e faz uma condicional com ela
+//  .then((r) => {
+  // r.erro retorna true ou false dependendo se tiver erro ou não diretamente da API  
+//    if(r.erro){
+//      console.log(r.erro)
+//        throw Error('Esse CEP não existe!')
+//    } else {
+//      console.log(r)
+//   }
+//    })
+  // catch é para pegar o erro e dar um console.log caso exista o erro  
+//  .catch(erro => console.log(erro))
+  // finally é o finalmente, ou seja quando acabar a promessa retorna o que estiver ai denro
+//  .finally(mensagem => console.log("Processameto concluído"));
+
+const cep = document.getElementById("cep")
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+async function buscaEndereco(cep) {
+  try {
+      const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const consultaCEPConvertida = await consultaCEP.json();
+      if (consultaCEPConvertida.erro) {
+          throw Error('CEP não existente!');
+      }
+
+      preencherCampos(consultaCEPConvertida);
+  } catch (erro) {
+      console.log(erro);
+  }
+}
+
+function preencherCampos(respostaCep) {
+  const enderecoInput = document.getElementById('endereco');
+  const bairroInput = document.getElementById('bairro')
+  const cidadeInput = document.getElementById('cidade')
+  const estadoInput = document.getElementById('estado')
+
+  enderecoInput.value = respostaCep.logradouro;
+  bairroInput.value = respostaCep.bairro;
+  cidadeInput.value = respostaCep.localidade;
+  estadoInput.value = respostaCep.uf;
+}
+
+
+
+
